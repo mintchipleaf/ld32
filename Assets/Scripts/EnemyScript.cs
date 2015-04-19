03faf3c23cb9d1de6420f46db7551b007026b03e
mintleaf <mintchipleaf@gmail.com>
2015-04-19 01:42:53 -0400

    Add logic for selfies to destroy everything in selfie camera

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -5,11 +5,13 @@ public class EnemyScript : MonoBehaviour {
 
 	private PhoneManager phone;
 	private bool markedVisible;
+	private float health;
 
 	// Use this for initialization
 	void Start () {
 		phone = PhoneManager.Instance;
 		markedVisible = false;
+		health = 100;
 	}
 	
 	// Update is called once per frame
@@ -20,6 +22,11 @@ public class EnemyScript : MonoBehaviour {
 	public bool MarkedVisible {
 		get {return markedVisible;}
 		set {markedVisible = value;}
+	}
+
+	public float Health {
+		get {return health;}
+		set {health = value;}
 	}
 
 	/*public void Kill() {
---
+++ Assets/Scripts/PhoneOverlayManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PhoneOverlayManager : MonoBehaviour {
+	public List<GameObject> Signals;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
